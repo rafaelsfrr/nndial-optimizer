53b48705b329bd465354626971f2f96a0ed52d0c
rafaelsfrr <rafael.sobfer@gmail.com>
2018-09-15 17:57:46 -0300

    Ajustes nos metodos de salvar e carregar o arquivo de configuracao

--- optimizer/util.py
+++ optimizer/util.py
@@ -2,22 +2,32 @@ import configparser
 import shutil
 
 
-def load_config_file(path):
+def load_config_file(path, section):
     """
     :param path: the path of the config file to be loaded
-    :return: the config file
+    :param section: the section of the config file that should be loaded
+    :return: the value of the config file section
     """
 
     config = configparser.ConfigParser()
-    return config.read(path)
+    config.read(path)
+    return [config[section][key] for key in config[section]]
 
 
-def write_config_file(config, path):
+def write_config_file(list, path):
     """
-    :param config: the config file
+    :param list: the list with arguments to be write
     :param path:  the path to save the file
     :return: none
     """
+
+    config = configparser.ConfigParser()
+
+    config.read('config/NDM.cfg')
+    config['learn']['lr'] = str(round(list[0], 3))
+    config['learn']['lr_decay'] = str(round(list[1], 2))
+    config['learn']['l2'] = str(round(list[2], 5))
+    config['learn']['random_seed'] = str(int(list[3]))
 
     with open(path, 'w') as configfile:
         config.write(configfile)
